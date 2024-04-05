f45f6a803fe5f0d5727348842a0d7a65d20025db
KusokTatarina <nickiforov.ni2013@yandex.ru>
2024-04-05 19:24:32 +0300

    add insert data

--- src/main.py
+++ src/main.py
@@ -1,10 +1,7 @@
-import asyncio
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from database import Base, sync_engine, sync_session
-from models import TypeDance
-from orm import async_dalete_tables, sync_create_tables, async_create_tables, view_all
+from orm import async_dalete_tables, insert_data, async_create_tables, view_all
 from router_type_dance.router_dance import router as router_dance
 from router_coach.router_coach import app as router_coach
 from router_collective.router_collective import app as router_collective
@@ -12,8 +9,9 @@ from router_person.router_person import app as router_person
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # await async_dalete_tables()
-    # await async_create_tables()
+    await async_dalete_tables()
+    await async_create_tables()
+    await insert_data()
     yield
 
 
--- src/orm.py
+++ src/orm.py
@@ -1,13 +1,12 @@
-from sqlalchemy import insert, text, select
-from sqlalchemy.orm import joinedload, relationship
+from sqlalchemy import select
 
-from database import sync_engine, async_engine, sync_session, async_session, Base
-from models import Coach, Collective, Person, TypeDance
+from database import async_engine, async_session, Base
+from models import Coach, Collective, Person, Rank, TypeDance
 
-def sync_create_tables():
-    sync_engine.echo = True
-    Base.metadata.drop_all(sync_engine)
-    Base.metadata.create_all(sync_engine)
+# def sync_create_tables():
+#     sync_engine.echo = True
+#     Base.metadata.drop_all(sync_engine)
+#     Base.metadata.create_all(sync_engine)
 
 
 async def async_create_tables():
@@ -18,6 +17,43 @@ async def async_create_tables():
 async def async_dalete_tables():
         async with async_engine.begin() as conn:
             await conn.run_sync(Base.metadata.drop_all)
+
+async def insert_data():
+     async with async_session() as session:
+        #   type_dance = [
+        #        {'name': 'Классический балет'},
+        #        {'name': 'Современный балет'}
+        #   ]
+        #   stmt = insert(TypeDance).values(type_dance)
+
+        type_dance1 = TypeDance(name='Классический балет') # id:1
+        type_dance2 = TypeDance(name='Современный балет') # id:2
+
+        coach1 = Coach(name='Иван Иванов Иванович', type_dance=1) # id:1
+        coach2 = Coach(name='Гарик Гариков Гарикович', type_dance=2) # id:2
+
+        collective1 = Collective(name='Очень классные ребята', name_coach=1, founded=2001)
+        collective2 = Collective(name='Не очень классные ребята', name_coach=2, founded=2074)
+
+        person1 = Person(lastname='Никифоров', collective_id=1, 
+                        photo=r'C:\Users\nicki\Desktop\VanyaProject\coach_pictures\9.jpg', 
+                        town='Псков', rank=Rank.M, scores=100)
+        person2 = Person(lastname='Попов', collective_id=2,
+                        photo=r'C:\Users\nicki\Desktop\VanyaProject\coach_pictures\scale_1200.jpg',
+                        town='Солярка', rank=Rank.E, scores=0)
+        
+        session.add_all([type_dance1, type_dance2])
+        await session.commit()
+        
+        session.add_all([coach1, coach2])
+        await session.commit()
+
+        session.add_all([collective1, collective2])
+        await session.commit()
+
+        session.add_all([person1, person2])
+        await session.commit()
+          
 
 
 async def return_one_object(models, id:int):
